4fd5eb2a8db1b3a2b4cfd958fc00f4a4de75b1c5
Asiaviscardi <asia.viscardii@gmail.com>
2023-11-17 16:11:42 +0100

    traferimento codice

---
+++ resources/views/home.blade.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="barra flex">
+
+<div id="categorie pad">
+
+    <ul class="flex">
+
+        <li><a href="">Donna</a></li>
+
+        <li><a href="">Uomo</a></li>
+
+        <li><a href="">Bambini</a></li>
+
+    </ul>
+
+</div>
+
+<div class="lb pad flex">
+
+    <img id="logo" src="../../../src/assets/booleanlogo.png" alt="">
+
+</div>
+
+<div class="funzioni pad">
+
+
+</div>
+
+</div>
+
+
+<div class="container flex">
+
+<div class="card flex">
+
+    <img src="../../../public/favicon.ico" alt="">
+
+    <div class="cuore">
+
+        <p>&hearts;</p>
+
+    </div>
+
+    <div class="sconto">30%</div>
+
+    <div class="sos">Sostenibilità</div>
+
+    <div class="specifiche">
+
+        <div class="marca">Levi's</div>
+
+        <h3 class="descrizione">RELAXED FIT TEE UNISEX</h3>
+
+        <div class="prezzo">14.99 &euro; <span>29,99 &euro;</span></div>
+
+    </div>
+
+</div>
+
+<div class="card flex">
+
+<img src="../../../public/favicon.ico" alt="">
+
+<div class="cuore">
+
+    <p>&hearts;</p>
+
+</div>
+
+<div class="sconto">30%</div>
+
+<div class="specifiche">
+
+    <div class="marca">Guess</div>
+
+    <h3 class="descrizione">ROSES TEE</h3>
+
+    <div class="prezzo">20.99 &euro; <span>29,99 &euro;</span></div>
+
+</div>
+
+</div>
+
+<div class="card flex">
+
+<img src="../../../public/favicon.ico" alt="">
+
+<div class="cuore">
+
+    <p>&hearts;</p>
+
+</div>
+
+<div class="sconto">30%</div>
+
+<div class="specifiche">
+
+    <div class="marca">Come Zucchero Filato</div>
+
+    <h3 class="descrizione">VOGLIA DI COLORI PASTELLO</h3>
+
+    <div class="prezzo">129.99 &euro; <span>184,99 &euro;</span></div>
+
+</div>
+
+</div>
+
+<div class="card flex">
+
+<img src="../../../public/favicon.ico" alt="">
+
+<div class="cuore">
+
+    <p>&hearts;</p>
+
+</div>
+
+<div class="sconto">50%</div>
+
+<div class="sos">Sostenibilità</div>
+
+<div class="specifiche">
+
+    <div class="marca">Levi's</div>
+
+    <h3 class="descrizione">TEE UNISEX</h3>
+
+    <div class="prezzo">14.99 &euro; <span>29,99 &euro;</span></div>
+
+</div>
+
+</div>
+
+<div class="card flex">
+
+<img src="../../../public/favicon.ico" alt="">
+
+<div class="cuore">
+
+    <p>&hearts;</p>
+
+</div>
+
+<div class="specifiche">
+
+    <div class="marca">Maya Deluxe</div>
+
+    <h3 class="descrizione">STRIPE BODICE</h3>
+
+    <div class="prezzo">99.99 &euro;</div>
+
+</div>
+
+</div>
+
+<div class="card flex">
+
+<img src="../../../public/favicon.ico" alt="">
+
+<div class="cuore">
+
+    <p>&hearts;</p>
+
+</div>
+
+<div class="sos">Sostenibilità</div>
+
+<div class="specifiche">
+
+    <div class="marca">Espnt</div>
+
+    <h3 class="descrizione">MAGLIONE - BLACK</h3>
+
+    <div class="prezzo">29.99 &euro;</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+
+<div class="inferiore flex">
+
+<div class="ineriore-info flex">
+
+    <div id="info">
+
+        <h4>Booleando s.r.l</h4>
+
+        <div class="info-box flex">
+
+            <span>Prova</span>
+
+            <span>Prova</span>
+
+            <span>Prova</span>
+
+        </div>
+
+    </div>
+
+    <div id="social">
+
+        <h4>Trovaci anche su</h4>
+
+        <div class="info-box flex">
+
+            <i class="fa-brands fa-square-twitter"></i>
+            <i class="fa-brands fa-square-twitter"></i>
+            <i class="fa-brands fa-square-twitter"></i>
+            <i class="fa-brands fa-square-twitter"></i>
+            <i class="fa-brands fa-square-twitter"></i>
+
+        </div>
+
+    </div>
+
+</div>
+
+</div>
+</body>
+</html>
